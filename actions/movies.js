f969f5aae4f8af6feace2a75632b0feaa28ac815
rukshan2004lk <sandeesharukshan321@gmail.com>
2025-06-06 16:44:35 +0530

    Create add new movie feature

--- actions/movies.js
+++ actions/movies.js
@@ -1,28 +1,50 @@
+"use server";
+import { db } from "@/lib/db";
 // get all movies action
 export const getMovies = async () => {
-    try {
-      // using fetch API to get movies from the server
-      const response = await fetch("http://localhost:3000/api/v1/movies", {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-          Accept: "application/json",
-        },
-        cache: "no-store",
-      });
-  
-      if (!response.ok) {
-        throw new Error("Network response was not ok");
-      }
-  
-      if (response.status === 200) {
-        return await response.json();
-      } else {
-        console.log("No movies found!");
-        return undefined;
-      }
-    } catch (error) {
-      console.log("Error fetching movies:", error);
+  try {
+    // using fetch API to get movies from the server
+    const response = await fetch("http://localhost:3000/api/v1/movies", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      cache: "no-store",
+    });
+
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+
+    if (response.status === 200) {
+      return await response.json();
+    } else {
+      console.log("No movies found!");
       return undefined;
     }
-  };
+  } catch (error) {
+    console.log("Error fetching movies:", error);
+    return undefined;
+  }
+};
+
+// create movie action
+
+export const createMovie = async (movie) => {
+  try {
+    const result = await db.collection("movies_n").insertOne(movie);
+
+    if (result.acknowledged) {
+      console.log(`A movie was inserted with the _id: ${result.insertedId}`);
+      return {
+        success: true,
+        message: "Movie created successfully!",
+      };
+    } else {
+      return undefined;
+    }
+  } catch {
+    console.log("Mongodb insert failed!");
+  }
+};
--- app/admin/movies/page.jsx
+++ app/admin/movies/page.jsx
@@ -1,3 +1,17 @@
-export default function Moviepage() {
-  return <div>Moviepage</div>;
+import AddMovieDialog from "./add-movie-dialog";
+
+export default function Moviespage() {
+  // space-y-4: 16px
+ return (
+    <div className="space-y-4">
+      <div className="flex items-center justify-between">
+        <div>
+          <h2 className="text-3xl font-bold tracking-tight">Movies</h2>
+          <p className="text-muted-foreground">Manage your movie catalog</p>
+        </div>
+
+        <AddMovieDialog />
+      </div>
+    </div>
+  );
 }
