7912153c5bbcc2bcc8a293014d124e64bea3b55c
Sandeesha Rukshan <sandeesharukshan321@gmail.com>
2025-06-23 23:23:27 +0530

    Improved movie loading page loading

--- app/movies/[id]/page.jsx
+++ app/movies/[id]/page.jsx
@@ -1,21 +1,29 @@
 import { getMovieById } from "@/actions/movies";
+import MovieLoading from "./movie-loading";
 
 export async function generateMetadata(props) {
-  const {id} = await props.params;
+  const { id } = await props.params;
   const movie = await getMovieById(id);
 
-  return{
-    title: movie?.data?.title ? `CineScope | ${movie.data.title}` : "CineScope | Movie Details",
-    description: movie?.data?.plot ?? "Find your favorite movie ratings and recommendations "
-  }
-  
+  return {
+    title: movie?.data?.title
+      ? `CineScope | ${movie.data.title}`
+      : "CineScope | Movie Details",
+    description:
+      movie?.data?.plot ??
+      "Find your favorite movie ratings and recommendations ",
+  };
 }
 //server component
 export default async function MovieDetailsPage(props) {
   const { id } = props.params;
+  //Simulate a delay for demonstrations 2 sec
+  await new Promise((resolve) => setTimeout(resolve, 2000));
   const movie = await getMovieById(id);
 
+  //throw new Error("Movie not found");
   //console.log("Movie", movie);
+
   return (
     <main className="flex flex-col justify-center py-16 px-4 mx-auto">
       <h1 className="font-bold text-center text-xl text-amber-600">
@@ -25,6 +33,8 @@ export default async function MovieDetailsPage(props) {
       <h2 className="text-center py-5">Movie: ID: {id}</h2>
       <h2 className="text-center py-5">Movie Title: {movie?.data?.title}</h2>
       <p className="text-center py-5">Movie Plot: {movie?.data?.plot}</p>
+
+      <MovieLoading />
     </main>
   );
 }
