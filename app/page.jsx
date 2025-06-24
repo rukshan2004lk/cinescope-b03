09e4d87acc71f6f42896c50a1ef100eb55475310
Sandeesha Rukshan <sandeesharukshan321@gmail.com>
2025-06-25 00:27:53 +0530

    Update footer

--- app/page.jsx
+++ app/page.jsx
@@ -2,8 +2,6 @@ import HeaderNav from "@/components/header-nav"; //alias import
 import FeaturedMovies from "@/components/home/featured-movies";
 import HeroBanner from "@/components/home/hero-banner";
 
-
-
 //SSR - Server Side Rendered - Server Component
 export default function Home() {
   return (
@@ -13,7 +11,12 @@ export default function Home() {
         <HeroBanner />
         <FeaturedMovies />
       </main>
-      <footer className=" bg-amber-400 h-72">Footer Section</footer>
+      <footer className="bg-amber-400 text-center text-gray-800 py-6">
+        <p className="text-lg font-semibold">
+          © 2025 CineScope
+        </p>
+        <p className="text-sm">All rights reserved.</p>
+      </footer>
     </div>
   );
 }
